6bbfd2c6133333c75eb7efc5ec518ddf3db56d38
reddythedev <officialreddy20@gmail.com>
2023-05-20 15:10:37 +0200

    threading

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
-const axios = require('axios');
+const { resolve } = require('path');
+const request = require('request');
 require('dotenv').config();
 
 function removeLinesWithKeyword(filename, keyword) {
@@ -15,96 +16,114 @@ const subject = process.env.TITLE;
 const message = process.env.MESSAGE;
 const proxies = fs.readFileSync(process.env.PROXIES, 'utf-8').trim().split('\n');
 const cookies = fs.readFileSync(process.env.COOKIES, 'utf8').split('\n');
+let rateLimited = false;
 
 let currentCookieIndex = 0;
 let messagesSent = 0;
 
 if (cookies.length < 2) {
   console.log('Not enough cookies! You need to have at least 2 verified cookies.');
-  process.exit(1);
-}
+};
 
 function getNextProxy() {
   const proxy = proxies[proxyIndex];
   proxyIndex = (proxyIndex + 1) % proxies.length;
   return proxy;
-}
+};
 
 const sleepFunc = (ms) => {
   return new Promise(resolve => setTimeout(resolve, ms));
 };
-
+ 
 const makeRequests = async () => {
   const ids = fs.readFileSync(process.env.IDS, 'utf8').split('\n');
+  for (const id of ids) {
+    if (rateLimited == true) {
+      await sleepFunc(parseInt(process.env.RATE_LIMIT_WAIT));
+      rateLimited = false;
+    } else {
+      makeRequest(id.trim());
+      currentCookieIndex = (currentCookieIndex + 1) % cookies.length;
+      await sleepFunc(parseInt(process.env.COOLDOWN));
+    }
+  }
+}
 
-  const requests = ids.map(id => makeRequest(id.trim()));
-  await Promise.all(requests);
-};
-
-console.log(`Remember that if you have cookies and they are not verified, the program will keep sending the messages but they won't actually be received!`);
+console.log(`Remember that if you have cookies and they are not verified, program will keep sending the messages but they won't be actually recieved!`);
 sleepFunc(3000);
 
 const makeRequest = async (id) => {
-  let thisIndex = currentCookieIndex;
-  let token = '';
-  const getToken = async () => {
-    const url = `https://auth.roblox.com/v2/login`;
-    const options = {
-      url: url,
-      method: 'POST',
-      headers: {
-        "Content-Type": "application/json",
-        'cookie': `.ROBLOSECURITY=${cookies[thisIndex].trim()}`
+      if (rateLimited) {
+        return;
       }
+      let token = '';
+      let currentIndex = currentCookieIndex;
+      const getToken = async () => {
+        const url = `https://auth.roblox.com/v2/login`;
+        const options = {
+          url: url,
+          method: 'POST',
+          headers: {
+            "Content-Type": "application/json",
+            'cookie': `.ROBLOSECURITY=${cookies[currentIndex].trim()}`
+          }
+        };
+      
+        return new Promise((resolve, reject) => {
+          request(options, (error, response) => {
+            if (error) {
+              console.error('Error getting token:', error);
+              resolve();
+            } else {
+              token = response.headers['x-csrf-token'];
+              resolve();
+            }
+          });
+        });
+      };
+      await getToken();
+      console.log(`Making request for id: ${id} with token: ${token}`);
+      const url = `https://privatemessages.roblox.com/v1/messages/send`;
+      let proxy = getNextProxy();
+      const payload = {
+        subject: subject,
+        body: message,
+        recipientid: id
+      };
+      const options = {
+        url: url,
+        method: 'POST',
+        json: payload,
+        proxy: `http://${proxy}`,
+        headers: {
+          'User-Agent': 'Mozilla/5.0',
+          "x-csrf-token": token,
+          "Content-Type": "application/json",
+          'cookie': `.ROBLOSECURITY=${cookies[currentIndex].trim()}`
+        }
+      };
+      request(options, async (error, response, body) => {
+        if (error) {
+          console.log(`Error making request for id ${id}:`, error);
+          makeRequest(id);
+        } else if (response.statusCode == 429) {
+          console.log('Oh no, ratelimit exceeded.');
+          rateLimited = true;
+          await sleepFunc(parseInt(process.env.RATE_LIMIT_WAIT));
+          makeRequest(id);
+        } else if (response.statusCode == 200) {
+          removeLinesWithKeyword(process.env.IDS, id)
+          messagesSent++;
+          console.log(`Successfully sent message to id ${id}. Messages sent: ${messagesSent}. Response: ${response.statusMessage}`);
+        } else if (response.statusCode == 403) {
+          console.log('Token or auth problem, retrying...');
+          makeRequest(id);
+        } else {
+          console.log(`Unexpected response for id ${id}:`, response.statusCode, body);
+          makeRequest(id);
+        }
+      });
     };
-
-    try {
-      const response = await axios(options);
-      token = response.headers['x-csrf-token'];
-    } catch (error) {
-      console.error('Error getting token:', error);
-    }
-  };
-  await getToken();
-  const url = `https://privatemessages.roblox.com/v1/messages/send`;
-  let proxy = getNextProxy();
-  const payload = {
-    subject: subject,
-    body: message,
-    recipientid: id
-  };
-  const options = {
-    url: url,
-    method: 'POST',
-    data: payload,
-    proxy: `http://${proxy}`,
-    headers: {
-      'User-Agent': 'Mozilla/5.0',
-      "x-csrf-token": token,
-      "Content-Type": "application/json",
-      'cookie': `.ROBLOSECURITY=${cookies[thisIndex].trim()}`
-    }
-  };
-  console.log(`Making request for id ${id} with token ${token}`);
-  try {
-    const response = await axios(options);
-    if (response.status == 429) {
-      console.log('Oh no, ratelimit exceeded.');
-    } else if (response.status == 200) {
-      removeLinesWithKeyword(process.env.IDS, id);
-      messagesSent++;
-      console.log(`Successfully sent message to id ${id}. Messages sent: ${messagesSent}. Response: ${response.statusText}`);
-    } else if (response.status == 403) {
-      console.log('Token or auth problem, retrying...');
-    } else if (response.status == 500) {
-      removeLinesWithKeyword(process.env.IDS, id);
-    } else {
-      console.log(`Unexpected response for id ${id}:`, response.status, response.data);
-    }
-  } catch (error) {
-    console.log(`Error making request for id ${id}:`, error.message);
-  }
-};
 
 makeRequests()
   .then(() => {
